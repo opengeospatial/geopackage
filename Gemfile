ab1dc120737c07755269dde36b1d253cb6a8ee7d
Joe Brumley <joeb@monkey.ogcinc.org>
2016-02-16 12:35:05 -0500

    Revert "Merge branch 'master' into gh-pages"
    This reverts commit 5d8b7f77dcfa2b7bdefd0a8640eed217d568bd30, reversing
    changes made to bd1d10fc28092c109f7864fc70e4c8ca70bacc20.

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'rake', '>= 0.9.2'
-gem 'asciidoctor', '~> 0.1.4'
-gem 'coderay', '1.0.9'
-gem 'tilt'
--- extensions/asciidoctor_extensions.rb
+++
@@ -1,7 +0,0 @@
-require 'asciidoctor'
-require File.dirname(__FILE__) + '/asciidoctor_patch'
-require File.dirname(__FILE__) + '/requirement_block'
-
-Asciidoctor::Extensions.register do |document|
-  block :requirement, RequirementBlock
-end
